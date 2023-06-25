fa1c55e5239a2d68675131e0168010c5f5243058
Vanderbruggen Tristan <vanderbruggentristan@gmail.com>
2023-06-25 21:31:28 +0000

    Started mocking out the server

--- autocog/__main__.py
+++ autocog/__main__.py
@@ -3,8 +3,13 @@ import os, sys, json
 import asyncio
 import argparse
 
+try:
+    import flask
+except:
+    flask = None
+
 from autocog import CogArch
-from autocog.architecture import PromptTee
+from autocog.architecture.utility import PromptTee
 
 import autocog.lm
 
@@ -12,31 +17,40 @@ def argparser():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--version',  action='version', version=f'AutoCog v0.1.0') # TODO `autocog.version:str=read('VERSION')`
 
-    parser.add_argument('--prefix',   help="""String to identify this instance of AutoCog (used when displaying and saving the prompts)""")
-    parser.add_argument('--tee',      help="""Filepath or `stdout` or `stderr`. If present, prompts will be append to that file as they are executed.""")
-    parser.add_argument('--fmt',      help="""Format string used to save individual prompts to files. If present but empty (or `default`), `results/{p}/{c}/{t}-{i}.txt` is used. `p` is the prefix. `c` is the sequence id of the call. `t` is the prompt name. `i` is the prompt sequence id. WARNING! This will change as the schema is obsolete!""")
-    parser.add_argument('--opath',    help="""If present, JSON outputs will be stored in that file. If missing, JSON output are written to stdout.""")
-
     parser.add_argument('--lm',       action='append', help="""Inlined JSON or path to a JSON file: `{ 'text' : { 'cls' : "OpenAI", ... } }` see TODO for details.""")
     parser.add_argument('--program',  action='append', help="""Inlined JSON or path to a JSON file: `{ 'writer' : { 'filepath' : './library/writer/simple.sta', ... } }` see TODO for details.""")
     parser.add_argument('--tool',     action='append', help="""Inlined JSON or path to a JSON file: `{ 'search' : { 'cls' : "SerpApi", ... } }` see TODO for details.""")
+
+    parser.add_argument('--prefix',   help="""String to identify this instance of AutoCog (used when displaying and saving the prompts)""", default='autocog')
+    parser.add_argument('--tee',      help="""Filepath or `stdout` or `stderr`. If present, prompts will be append to that file as they are executed.""")
+    parser.add_argument('--fmt',      help="""Format string used to save individual prompts to files. If present but empty (or `default`), `results/{p}/{c}/{t}-{i}.txt` is used. `p` is the prefix. `c` is the sequence id of the call. `t` is the prompt name. `i` is the prompt sequence id. WARNING! This will change as the schema is obsolete!""")
+
+    parser.add_argument('--host',     help="""Host for flask server.""", default='localhost')
+    parser.add_argument('--port',     help="""Port for flask server.""", default='5000')
+    parser.add_argument('--debug',    help="""Whether to run the flask server in debug mode.""", action='store_true')
+
+
     parser.add_argument('--command',  action='append', help="""Inlined JSON or path to a JSON file: `{ 'callee' : 'writer',  ... }` see TODO for details.""")
+    parser.add_argument('--opath',    help="""If present, JSON outputs of the commands will be stored in that file. If missing, they are written to stdout.""")
     
     return parser
 
-def parseargs(args):
+def parseargs(argv):
     parser = argparser()
     args = parser.parse_args(argv)
     
     LMs = {}
-    for lm in args.lm:
-        LMs.update(lm)
+    if args.lm is not None:
+        for lm in args.lm:
+            LMs.update(lm)
     programs = {}
-    for prog in args.program:
-        programs.update(prog)
+    if args.program is not None:
+        for prog in args.program:
+            programs.update(prog)
     tools = {}
-    for tool in args.tool:
-        tools.update(tool)
+    if args.tool is not None:
+        for tool in args.tool:
+            tools.update(tool)
 
     pipe_kwargs = { 'prefix' : args.prefix }
     if args.tee is not None:
@@ -51,13 +65,26 @@ def parseargs(args):
         else:
             pipe_kwargs.update({ 'fmt' : args.fmt })
 
-    return ({
-        'arch' : CogArch(pipe=PromptTee(pipe_kwargs)),
-        'LMs' : LMs, 'programs' : programs, 'tools' : tools,'commands' : args.command
-    }, args.opath)
-    
+    return {
+        'arch' : CogArch(pipe=PromptTee(**pipe_kwargs)), 'opath' : args.opath,
+        'LMs' : LMs, 'programs' : programs, 'tools' : tools, 'commands' : args.command,
+        'flask_host' : args.host, 'flask_port' : int(args.port), 'flask_debug' : args.debug
+    }
+
+def autocog_server_index():
+    return 'hello world !!!'
+
+def autocog_server_register():
+    raise NotImplementedError() # TODO register a tool
+
+def autocog_server_load():
+    raise NotImplementedError() # TODO load a program
+
+def autocog_server_execute():
+    raise NotImplementedError() # TODO execute a `cog`
+
 # TODO method of CogArch?
-async def run(arch, LMs, programs, tools, commands):
+async def run(arch, LMs, programs, tools, commands, opath, flask_host, flask_port, flask_debug):
     for fmt in list(LMs.keys()):
         desc = LMs[fmt]
         cls = desc["cls"]
@@ -67,26 +94,30 @@ async def run(arch, LMs, programs, tools, commands):
         LMs.update({ fmt : cls(**desc) })
     arch.orchestrator.LMs.update(LMs)
 
-    for tag in list(programs.keys()):
-        arch.load(tag=tag, **programs[tag])
+    for (tag,program) in programs.items():
+        arch.load(tag=tag, **program)
 
-    for tag in list(tools.keys()):
+    for (tag,tool) in tools.items():
         raise NotImplementedError()
 
-    res = await asyncio.gather(*[
-        arch(tag, **inputs)
-        for (tag,inputs) in commands
-    ])
-    return [ r[0] for r in res ]
+    if commands is not None:
+        res = await asyncio.gather(*[
+            arch(tag, **inputs)
+            for (tag,inputs) in commands
+        ])
 
-def main(argv=sys.argv[1:]):
-    (kwargs,opath) = parseargs(argv)
-    res = asyncio.run(run(**kwargs))
-    if output is None:
-        print(json.dumps(res, indent=4))
+        if opath is None:
+            print(json.dumps(res, indent=4))
+        else:
+            with open(opath,'w') as F:
+                json.dump(res, F, indent=4)
     else:
-        with open(opath,'w') as F:
-            json.dump(res, F, indent=4)
+        app = flask.Flask(__name__, template_folder=os.path.realpath('share/webapp')) # TODO install ressources with pip
+        app.add_url_rule('/',         view_func=autocog_server_index)
+        app.add_url_rule('/register', view_func=autocog_server_register)
+        app.add_url_rule('/load',     view_func=autocog_server_load)
+        app.add_url_rule('/execute',  view_func=autocog_server_execute)
+        app.run(host=flask_host, port=flask_port, debug=flask_debug)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(run(**parseargs(argv=sys.argv[1:])))
